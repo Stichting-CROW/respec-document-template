a5ac428d695bab45188b26989fdc0bd2216be767
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-06-02 12:27:38 +0200

    feat: add license field
    Reduces prompts and adds some help info.
    
    Fixes #2

--- {{cookiecutter.document_slug}}/config.js
+++ {{cookiecutter.document_slug}}/config.js
@@ -1,13 +1,14 @@
 var respecConfig = {
   // => https://github.com/stichting-crow/respec/wiki
-  specType: "{{ cookiecutter.specType }}", // vrije tekst
-  specStatus: "{% if cookiecutter.on_review_track == 'true' %}WD{% else %}DRAFT{% endif %}", // kies uit https://github.com/Stichting-CROW/respec/wiki/specStatus
-  imprint: "crow", // Kies uit http://docs.crow.nl/respec-design/stichting-crow/#logos
-  shortName: "{{ cookiecutter.repository_name }}/{{ cookiecutter.document_slug }}",
+  specStatus: "{% if cookiecutter.on_review_track == 'true' %}WD{% else %}DRAFT{% endif %}",
+  shortName: "{{ cookiecutter.repository_name }}/{{ cookiecutter._document_slug }}",
+  specType: "{{ cookiecutter._spec_type }}", // vrije tekst
+  imprint: "{{ cookiecutter.publisher }}",
   editors: [
     {
       name: "{{ cookiecutter.first_editor }}", // verplicht, de rest is optioneel
     },
   ],
   github: "https://github.com/Stichting-CROW/{{ cookiecutter.repository_name }}",
+  license: "{{ cookiecutter.license }}",
 };
