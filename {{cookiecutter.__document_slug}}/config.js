51f2be778d9e705201fcb118194b18f71c813868
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2022-07-29 09:58:31 +0200

    docs: improve config.js comments

--- {{cookiecutter.__document_slug}}/config.js
+++ {{cookiecutter.__document_slug}}/config.js
@@ -2,11 +2,12 @@ var respecConfig = {
   // => https://github.com/stichting-crow/respec/wiki
   specStatus: "{% if cookiecutter.on_review_track == 'true' %}WD{% else %}DRAFT{% endif %}",
   shortName: "{{ cookiecutter.repository_name }}/{{ cookiecutter.__document_slug }}",
-  specType: "{{ cookiecutter.__spec_type }}", // vrije tekst
+  specType: "{{ cookiecutter.__spec_type }}", // vrije tekst, kleine letter
   imprint: "{{ cookiecutter.publisher }}",
   editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
     {
-      name: "{{ cookiecutter.first_editor }}", // verplicht, de rest is optioneel
+      name: "{{ cookiecutter.first_editor }}",
     },
   ],
   github: "https://github.com/Stichting-CROW/{{ cookiecutter.repository_name }}",
